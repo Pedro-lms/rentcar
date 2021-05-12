732136451cc13f8e6ce14757c172a467f6d849a8
Pedro Sadit <pedrodaluzmachado@gmail.com>
2021-05-12 16:27:09 -0300

    Initial Project

---
+++ src/main/java/com/geral/projeto/ProjetoGeralSpringApplication.java
@@ -0,0 +1,13 @@
+package com.geral.projeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoGeralSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoGeralSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/geral/projeto/ProjetoGeralSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geral.projeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoGeralSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
