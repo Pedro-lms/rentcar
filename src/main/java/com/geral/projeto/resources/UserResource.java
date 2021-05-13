3f1550c8cf4cd1fc2f0c61e3924b83230521f69b
Pedro Sadit <pedrodaluzmachado@gmail.com>
2021-05-12 21:23:47 -0300

    Service layer, component registration

--- src/main/java/com/geral/projeto/resources/UserResource.java
+++ src/main/java/com/geral/projeto/resources/UserResource.java
@@ -1,19 +1,33 @@
 package com.geral.projeto.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.geral.projeto.entities.User;
+import com.geral.projeto.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")  //Define o caminho do recurso de acordo com o mapeamento do objeto relacionado na localhost, ou no acesso
 public class UserResource {
 
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "Pedro", "pedrodaluzmachado@gmail.com", "988888", "9999");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
---
+++ src/main/java/com/geral/projeto/services/UserService.java
@@ -0,0 +1,28 @@
+package com.geral.projeto.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.geral.projeto.entities.User;
+import com.geral.projeto.repositories.UserRepository;
+
+@Service 
+public class UserService {
+
+	@Autowired //O Spring faz a injeção de forma automática e trasnparente
+	private UserRepository repository; //Injeção de dependência com a interface
+
+	public List<User> findAll(){
+		return repository.findAll(); //Chamando a lista para encontrar o serviço de repositório na Arquiterura do projeto
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
+
+//@Service Registra a classe como componente do spring, da mesma forma que o @Component, porém com uma semântica mais específica
